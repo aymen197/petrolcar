c5d05cb530da0584f9ee652709dae79ead091a06
aymen197 <aymenmuhammed2@gmail.com>
2026-04-26 17:45:06 +0300

    setup the project

---
+++ src/mynewclass.java
@@ -0,0 +1,5 @@
+public class mynewclass {
+public static void main(String[] args) {
+    System.out.println("my name is aymen mohammed yassin");
+}
+}
---
+++ src/petrolcar.java
@@ -0,0 +1,23 @@
+class Vehicle {
+    String brand = "OPEL";
+    int topSpeed = 180;
+
+    public void displaySpecs() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Top Speed: " + topSpeed + " km/h");
+    }
+}
+class EVCar extends Vehicle {
+    int batteryCapacity = 75;
+
+    public void showEVDetails() {
+        System.out.println("This " + brand + " has a " + batteryCapacity + "kWh battery.");
+    }
+}
+class Petrolcar {
+    public static void main(String[] args) {
+        EVCar myEv = new EVCar();
+        myEv.displaySpecs();
+        myEv.showEVDetails();
+    }
+}
